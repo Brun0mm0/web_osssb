369e6d2433a6216a29d8c44849cef3c35c9ce9ec
ProyectosOsssb <bprovenzano@osssb.com.ar>
2023-04-14 08:23:25 -0300

    add img

--- components/script.js
+++ components/script.js
@@ -1,13 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
+    // loop: true,
     // navigation: {
-    //     nextEl: '.swiper-button-next',
+        // nextEl: '.swiper-button-next',
     //     prevEl: '.swiper-button-prev',
     // },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
+    // autoplay: {
+        // delay: 10000,
+        // disableOnInteraction: false,
+      // },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
