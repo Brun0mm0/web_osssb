7bc685d9c588d219eb69b5cad43b42456c3d4067
Brun0mm0 <provenzanobruno@gmail.com>
2023-04-18 00:09:06 -0300

    imagenes

--- components/script.js
+++ components/script.js
@@ -1,13 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-    // loop: true,
-    // navigation: {
-        // nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-    // autoplay: {
-        // delay: 10000,
-        // disableOnInteraction: false,
-      // },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+      },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
